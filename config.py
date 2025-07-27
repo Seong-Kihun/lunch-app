b792f7c3da65c520a21e6faba6d427588a70d7e9
Seong Kihun <kihun.seong.official@gmail.com>
2025-07-27 09:57:45 +0900

    홈탭 헤더 높이 조정 - 90px에서 60px로 변경

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-
+ 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
