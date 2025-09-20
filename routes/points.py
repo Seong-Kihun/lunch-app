71e8044386b79c5b31ebfe319d090e8b43b51e6b
Seong-Kihun <kihun.seong.official@gmail.com>
2025-09-20 22:04:42 +0900

    Update points.py

--- routes/points.py
+++ routes/points.py
@@ -3,9 +3,15 @@
 """
 
 from flask import Blueprint
+from auth.utils import require_auth
 
 # 포인트 Blueprint 생성
 points_bp = Blueprint('points', __name__, url_prefix='/api/points')
+
+# 인증 미들웨어 적용
+@points_bp.before_request
+def _points_guard():
+    return require_auth()()
 
 @points_bp.route('/')
 def points_home():
