f97906c1dcefbbc0c85e3903120ad35eebac58dd
Seong-Kihun <kihun.seong.official@gmail.com>
2025-09-20 22:37:53 +0900

    Update parties.py

--- api/parties.py
+++ api/parties.py
@@ -18,7 +18,7 @@ parties_bp = Blueprint('parties', __name__, url_prefix='/api/parties')
 # 인증 미들웨어 적용
 @parties_bp.before_request
 def _parties_guard():
-    return require_auth()()
+    return require_auth()
 
 # 모델 import
 from flask import current_app
